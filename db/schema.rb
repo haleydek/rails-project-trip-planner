6671dac9d26936608842d3f5b67080be77c2cc90
haleydek <hcd09@live.com>
2019-08-07 19:20:43 -0500

    Create Destination resource and persist to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_193117) do
+ActiveRecord::Schema.define(version: 2019_08_08_001808) do
+
+  create_table "destinations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
