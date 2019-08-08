bac7a17dae041c455697eed1a1ac4af20b79aec6
haleydek <hcd09@live.com>
2019-08-08 09:54:58 -0500

    Build Trip form, #new, #create, #edit, #update

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,46 @@
 class TripsController < ApplicationController
+    def new
+        @trip = Trip.new
+    end
+
+    def create
+        @trip = Trip.new(trip_params)
+        if @trip.valid?
+            @trip.save!
+
+            redirect_to trip_path(@trip)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        #Must validate trip.users includes current_user
+        @trip = Trip.find(params[:id])
+    end
+
+    def update
+        #Must validate trip.users includes current_user
+        @trip = Trip.find(params[:id])
+        if @trip.update
+            redirect_to trip_path(@trip)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+    end
+
+    def index
+    end
+
+    def show
+    end
+
+    private
+
+    def trip_params
+        params.require(:trip).permit(:title, :start_date, :end_date)
+    end
 end
