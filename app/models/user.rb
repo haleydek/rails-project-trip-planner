bfdbb8d16cfc1cfb8764014cb7e4852013527a55
haleydek <hcd09@live.com>
2019-08-08 09:32:46 -0500

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :trips, through: :userstrips
 
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /@/ }
 
     has_secure_password
 
