c0e748c031161e5f6d0eed291af198b728935d62
haleydek <hcd09@live.com>
2019-08-14 09:30:41 -0500

    Check if session[:user_id] exists instead of current_user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
     def home
-        if current_user
+        if session[:user_id]
             redirect_to user_trips_path(current_user)
         else
             render :home
