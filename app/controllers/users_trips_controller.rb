f5c07f42133cdcf1b8d13d3eb3c7b238afac2f35
haleydek <hcd09@live.com>
2019-08-14 09:14:31 -0500

    Add comments to users_trips controller

--- app/controllers/users_trips_controller.rb
+++ app/controllers/users_trips_controller.rb
@@ -1,11 +1,14 @@
 class UsersTripsController < ApplicationController
     before_action :authentication_required
 
+    # link on trip#show page routes to this action
     def update
         users_trip = UsersTrip.find(params[:id])
         trip = Trip.find_by(id: users_trip.trip_id)
 
+        # check if current user is an admin for the trip
         if current_user.admin_status(trip)
+            # change :trip_admin boolean value to the opposite of its current value
             users_trip.trip_admin = !users_trip.trip_admin
             users_trip.save
 
