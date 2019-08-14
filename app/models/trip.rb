3ef851d76517010ae7f68e4e69d52d725dfd48b4
haleydek <hcd09@live.com>
2019-08-14 10:00:13 -0500

    Refactor find_users_trip_with_user

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,4 @@
 class Trip < ApplicationRecord
-    #ensure only trip_admin can delete a trip
     has_many :users_trips, dependent: :destroy
     has_many :users, through: :users_trips
     has_many :trips_destinations, dependent: :destroy
@@ -31,4 +30,9 @@ class Trip < ApplicationRecord
         trip_admins = self.users.where(id: (UsersTrip.where(trip_id: self.id, trip_admin: true)).pluck(:user_id))
         trip_admins.ids.include?(current_user.id)
     end
+
+    def find_users_trip_with_user(user)
+        self.users_trips.where(user_id: user.id).first.id
+    end
+
 end
